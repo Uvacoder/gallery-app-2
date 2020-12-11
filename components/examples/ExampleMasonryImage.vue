67fbfbd249da64d26d6bad4d78d6e368476747d4
zcdev <zoechangdev@gmail.com>
2020-12-11 13:05:16 -0800

    Draft

---
+++ components/examples/ExampleMasonryImage.vue
@@ -0,0 +1,97 @@
+<template>
+  <section>
+    <vue-masonry-wall :items="items" :options="options" @append="append">
+      <template v-slot:default="{item}">
+        <div class="Item">
+          <img :src="item.image"/>
+
+          <div class="Content">
+            <h5 class="text-ellipsis-1l">{{item.title}}</h5>
+            <p class="text-ellipsis-2l">{{item.content}}</p>
+          </div>
+        </div>
+      </template>
+    </vue-masonry-wall>
+  </section>
+</template>
+
+<script>
+  import VueMasonryWall from "vue-masonry-wall";
+
+  export default {
+    name: "ExampleMasonry",
+    components: {VueMasonryWall},
+    data() {
+      return {
+        options: {
+          width: 300,
+          padding: {
+            2: 8,
+            default: 12
+          },
+        },
+        items: [
+          {
+            title: 'Wood Series',
+            content: 'Clay Sculptures',
+            image: 'swood.jpg'
+          },
+          {
+            title: 'Abstract Forms',
+            content: 'Clay Sculptures',
+            image: 'pabstract.jpg'
+          },
+          {
+            title: 'Organic Forms',
+            content: 'Clay Sculptures',
+            image: 'sorganic.jpg'
+          },
+          {
+            title: 'Metal Series',
+            content: 'Clay Sculptures',
+            image: 'smetal.jpg'
+          },
+          {
+            title: 'Nature Series',
+            content: 'Paintings',
+            image: 'pnature.jpg'
+          },
+          {
+            title: 'Chicken Series',
+            content: 'Paintings',
+            image: 'pchickens.jpg'
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .Item {
+    overflow: hidden;
+    border-radius: 4px;
+    width: 100%;
+    background: #262626;
+  }
+
+  .Content {
+    padding: 20px;
+  }
+
+  h5 {
+    color: #D43790;
+  }
+
+  p {
+    color: #eee;
+  }
+
+  img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    line-height: 0;
+    display: block;
+  }
+</style>
---
+++ components/layout/NavHeader.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="Header">
+    <div class="container h-100 flex-align-center">
+      <nuxt-link to="/" class="lh-0 flex-align-center">
+        <logo width="32px"
+              height="32px"/>
+
+        <h2 class="ml-16">
+          PaoStudio.Art
+        </h2>
+      </nuxt-link>
+
+      <div class="flex-grow"/>
+
+      <div class="flex-wrap m--8">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NavHeader"
+  }
+</script>
+
+<style scoped lang="less">
+  .Header {
+    height: 64px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 10000;
+    background: #262626;
+  }
+
+  a {
+    text-decoration: none;
+    color: #D43790;
+  }
+
+  h2 {
+    color: #D43790;
+  }
+</style>
